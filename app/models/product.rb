f51836b058ceadd6075cced05de85a6a766f48b5
beelarr <bryonl@me.com>
2017-11-16 14:57:12 -0600

    Spec validations from issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ApplicationRecord
-  validates :quantity, numericality: { :greater_than_or_equal_to => 0  }
-  validates :product_name, :description, :price, :local, :exp_date, presence: true
-  validates :local, inclusion: { in: [true, false] }
+  validates :quantity, :price, numericality: { :greater_than_or_equal_to => 0  }
+  validates :product_name, :description, :price, :exp_date, presence: true
+  validate :exp_date_after_now?
+
+  def exp_date_after_now?
+    if exp_date
+      if exp_date < Date.today
+        errors.add :exp_date, "Your expiration date must be after today"
+      end
+    end
+  end
 
 end
