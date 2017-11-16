8391c53d289b61ddd8a1d220fccbef7066e06002
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-16 13:22:32 -0600

    stash

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
     def index
         @categories = Category.all
+        @products = Product.all
+    end
+
+    def sort_products
+        
     end
 end
