76a50278ed164e6495586d0835fe983b9223de73
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-16 14:51:33 -0600

    tests pass for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      flash[:success] = "You're all signed up!"
+      redirect_to '/'
     else
       render 'new'
     end
