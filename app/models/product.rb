165008ab60ada045c0f79e57623fffeefae41ded
beelarr <bryonl@me.com>
2017-11-16 11:12:16 -0600

    Add form for new product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :quantity > 0
-  validates :active, :product_name, :description, :price, :local, :exp_date, presence: true
-  validates :active, :local, inclusion: { in: [true, false] }
+  validates :product_name, :description, :price, :local, :exp_date, presence: true
+  validates :local, inclusion: { in: [true, false] }
 
 end
