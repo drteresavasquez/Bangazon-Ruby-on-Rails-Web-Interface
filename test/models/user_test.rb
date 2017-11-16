beb6dbb48363dd0371c1d13b57a9e4274028d192
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-16 10:45:11 -0600

    validation tests pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(first_name: 'Jeremy', last_name: 'Wells', email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test 'first name should be present' do
+    @user.first_name = '        '
+    assert_not @user.valid?
+  end
+
+  test 'last name should be present' do
+    @user.last_name = '        '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '        '
+    assert_not @user.valid?
+  end
+
+  test 'user first name should not be too long' do
+    @user.first_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'user last name should not be too long' do
+    @user.last_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'user email should not be too long' do
+    @user.email = 'a' * 246
+    assert_not @user.valid?
+  end
+
 end
