98b67005b18f51e3a3426931d2316c5f3a68bcf8
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-16 11:08:20 -0600

    tweaked the model associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 	has_many :products_votes
 	has_many :users, :through => :products_votes
 	belongs_to :categories
+	has_many :wish_list_products
     validates :seller_id, :category_id, :product_name, :presence => true
 
 end
