b6b046765a1f2bbb08763486c054a7af494f1257
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-17 16:32:12 -0600

    updated product creation form to include user ID

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,45 +1,41 @@
+  class ProductsController < ApplicationController
+    include SessionsHelper
+      
+    def show
+          @product = Product.find(params[:id])
+          @seller = User.find(current_user.id)
+    end
 
-class ProductsController < ApplicationController
-
-  # Show user products
+        # Show user products
   def index
-    @current_user_prods = Product.where(seller_id: 3)
+    @current_user_prods = Product.where(seller_id: current_user.id)
 
     # make a call to the OrdersProducts table and compare the product_id with the @current_user_prods.id, then count how may products are in each batch to get the total sold for each item
-
-    # @number_sold = Product.includes("orders").where(first_name: 'Ryan', orders: { status: 'received' }).count
     @number_sold = OrdersProduct.where(:product_id => @current_user_prods.ids).group(:product_id).count
-
-    
   end
-
-    def show
-        @product = Product.find(params[:id])
-        @seller = User.find(@product.seller_id)
+  
+    def new
+      @product = Product.new
+      @categories = Category.all
     end
-
-  def new
-    @product = Product.new
-    @categories = Category.all
-  end
-
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      flash[:success] = "Product was created!"
-      redirect_to @product
-    else
-      render 'new'
+  
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        flash[:success] = "Product was created!"
+        redirect_to @product
+      else
+        @categories = Category.all
+        render 'new'
+      end
+    end
+  
+    private
+  
+    def product_params
+      params.require(:product).permit(:product_name, :quantity, :description,
+                                   :price, :local, :category_id, :exp_date,
+                                    :active, :seller_id)
+  
     end
   end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:product_name, :quantity, :description,
-                                 :price, :local, :category_id, :exp_date,
-                                  :active => true)
-
-  end
-end
-
