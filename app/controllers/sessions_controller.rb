3ffb0b396ebc59a736063ce787ce46d3ac8f80cd
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-17 12:49:35 -0600

    logs in and logs out now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to '/'
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    user = nil
-    redirect_to '/'
+    log_out
+    redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "You're all signed up!"
       redirect_to '/'
     else
