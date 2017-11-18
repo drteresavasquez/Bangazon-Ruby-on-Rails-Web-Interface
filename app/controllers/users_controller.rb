6636eb385e91da7d3338582e112e664ae2529096
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-17 21:35:47 -0600

    updated add payment type and delete payment type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+    include SessionsHelper
         def index
             @users = User.all
         end
 
         def show
-          @user = User.find(params[:id])
-          @user_products = Product.where(seller_id: params[:id])
+          @user = User.find(current_user.id)
+          @user_products = Product.where(seller_id: current_user.id)
+          @payment_types = PaymentType.where(user_id: current_user.id)
+          @number_sold = OrdersProduct.where(:product_id => @user_products.ids).group(:product_id).count
         end
       
         def new
