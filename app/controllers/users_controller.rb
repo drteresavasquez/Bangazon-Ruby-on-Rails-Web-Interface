46243f5d688bc4e5e2d83663c7d84fb16298203f
beelarr <bryonl@me.com>
2017-11-19 12:05:17 -0600

    fixed error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
           @user = User.new(user_params)
           if @user.save
             flash[:success] = "You're all signed up!"
-            redirect_to '/'
+            redirect_to @user
           else
             render 'new'
           end
